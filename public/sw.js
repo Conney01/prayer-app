ce834631ac35da686fe578c35ca4f4ee5f8e8c8c
Aron Cornellious <aroncornelius38@gmail.com>
2026-08-24 11:30:17 +0300

    fix: bypass TS checking on service worker for PWA installability

---
+++ public/sw.js
@@ -0,0 +1,12 @@
+﻿// @ts-nocheck
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', () => {
+  // Pass-through fetch handler satisfying PWA installability criteria
+});
